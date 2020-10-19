1497c8bbb396634c7b45621c0c90b8c2e6fb26a9
Jon Shadforth <jon@carpediemsystems.co.uk>
2020-10-19 12:10:01 +0700

    Removed unusd files

--- CDS.RoslynPadScripting/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CDS.RoslynPadScripting")]
-[assembly: AssemblyDescription("C# scripting editor and support utilities")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("CDS.RoslynPadScripting")]
-[assembly: ComVisible(false)]
-[assembly: Guid("a13aaea3-5c27-44af-9b96-f0af5be5cdb8")]
