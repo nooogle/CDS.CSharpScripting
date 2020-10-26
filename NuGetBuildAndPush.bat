c4b80eaa1cc3a3d250a085a5c1059357924c027a
Jon Shadforth <jon@carpediemsystems.co.uk>
2020-10-26 09:36:04 +0700

    Added batch file for re-packing and pushing the NuGet package

--- NuGetBuildAndPush.bat
+++ NuGetBuildAndPush.bat
@@ -1,16 +1,8 @@
 @echo off
 
 rem Run this script after building the Release configuration.
-rem Make sure to enter the correct version number and NuGet 
-rem API KEY
 
-
-echo --------------------------------------------------------
-echo Building package
-echo --------------------------------------------------------
-
-.\packages\NuGet.CommandLine.5.7.0\tools\NuGet.exe  pack .\CDS.CSharpScripting\CDS.CSharpScripting.csproj -Prop Configuration=Release
-
+cd .\CDS.CSharpScripting
 
 
 echo --------------------------------------------------------
@@ -19,18 +11,43 @@ echo --------------------------------------------------------
 
 set /p ver="Enter version: "
 set /p api_key="Enter api_key: "
+
+
+echo.
+echo --------------------------------------------------------
+echo Refresh the nupsec package
+echo --------------------------------------------------------
+
+.\..\packages\NuGet.CommandLine.5.7.0\tools\NuGet.exe  pack CDS.CSharpScripting.csproj -Prop Configuration=Release -Version %ver%
+
+
+
+echo.
+echo --------------------------------------------------------
+echo Push
+echo --------------------------------------------------------
+
+set command=.\..\packages\NuGet.CommandLine.5.7.0\tools\NuGet.exe push CDS.CSharpScripting.%ver%.nupkg %api_key% -Source https://api.nuget.org/v3/index.json
+echo.
+echo Push command looks like this:
+echo.
+echo %command%
+echo.
+
 choice /M "Push the package?"
 IF ERRORLEVEL 2 goto done
 
+echo.
 echo --------------------------------------------------------
 echo pushing
 echo --------------------------------------------------------
 
-echo .\packages\NuGet.CommandLine.5.7.0\tools\NuGet.exe push CDS.CSharpScripting.%ver%.nupkg %api_key% -Source https://api.nuget.org/v3/index.json
-
+echo.
+%command%
 
 :done
 
+echo.
 echo --------------------------------------------------------
 echo done
 echo --------------------------------------------------------
